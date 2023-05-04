dfa102034797460d23eb67beba0a7c5cdd045123
Kiyarash <kiarashnadri@yahoo.com>
2023-05-04 13:19:22 +0330

    initial commit for exercise file

---
+++ exercise2.js
@@ -0,0 +1,162 @@
+/*
+// 2-Unique Values JavaScript (ES6) :(new Set)
+fetch('./products.json').then((res) => res.json()).then((response) => myFunctin(response))
+function myFunctin(data) {
+    console.log(data)
+
+    names = data.map((item) => item.name);
+    console.log(names);//Array with 12 object
+
+    uniqueNames = new Set(names);
+    console.log(uniqueNames)//10 Object(Set)
+
+    //spread operator iterate over items of object
+    myArray = [...uniqueNames]
+    console.log(myArray);//Array with 10 element
+
+    myArray1 = [uniqueNames];
+    console.log(myArray1)//Array with 1 element that this element is uniqueNames object
+
+    myNewArry = ['newItem', ...uniqueNames]
+    console.log(myNewArry);//insert newItem first of array
+
+    const demo = document.querySelector('.list');
+    demo.innerHTML = myNewArry.map((dm) => {
+        return `<li>${dm}</li>`
+    }).join('');
+    //join for delete ,
+}
+*/
+// fetch('./products.json').then(res => res.json()).then(response => showData(response));
+
+// function showData(data) {
+//     const staff = data;
+//     //always return your fisrt parameter :
+//     const dailyTotal = staff.reduce((total, person) => {
+//         console.log('total is : ', total);
+//         console.log('person.price is : ', person.price);
+//         console.log('------------------------');
+//         total += person.price;
+//         return total;
+//         // return total += person.price;
+//     }, 0)//here 0 is value of total in first iteration
+
+//     console.log(dailyTotal)
+// }
+/*
+//here we dont have initial value 0 instead we have initial value of on object for reduce :
+function showData(data) {
+    const cart = data;
+    let { totalItems, cartTotal } = cart.reduce((total, cartItem) => {
+        //here total is this object : {totalItems: 0, cartTotal: 0}
+
+        const { age, price } = cartItem;
+        console.log('total is : ', total)
+        //count items :
+        total.totalItems += age;
+        total.cartTotal += age * price;
+        return total;
+    }, {
+        totalItems: 0,
+        cartTotal: 0
+    })
+    //in above second paramter is an object that have 2 item that have initial value 0 in first iteration.
+    cartTotal = Number(cartTotal.toFixed(2));
+    //wiht toFixed(2) after we have 2 number after .
+    console.log(totalItems, cartTotal);
+}
+//*note : vaghti mikhay y object mesle const {name, age}= y object dg bezari to oon yki object bayad hatamn y property b esme age o name dashte bashi
+*/
+/*
+//Destructuring (Array):
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
+
+const fruits1 = fruits[0];
+const fruits2 = fruits[1];
+const fruits3 = fruits[2];
+
+console.log(fruits1, fruits2, fruits3);
+
+const [john, peter11, bob, , kelly, susan] = friends;
+console.log(john, peter11, bob, kelly, susan);
+
+//easier way to swap between 2 item of array :
+let first = 'bob';
+let second = 'john';
+
+// let temp = second;
+// second = first;
+// first = temp;
+
+[second, first] = [first, second];
+
+console.log(first, second)
+*/
+
+//Rest Operator :
+//Array
+const fruits = ["apple", "orange", "lemon", "banana"];
+const [fisrt, ...restOfTheFruits] = fruits;
+console.log(fisrt); //apple
+console.log("rest", restOfTheFruits); //[ "orange", "lemon", "banana"]
+const specificFruit = restOfTheFruits.find((fruit) => fruit === "lemon");
+console.log(specificFruit); //lemon
+//objects
+const person = { name: "john", lastName: "smith", job: "developer" };
+// const {...rest,name}=person//this don't work
+const { name, ...rest } = person;
+console.log(name); //john
+console.log(rest); //{lastName: 'smith', job: 'developer'}
+
+//functions :
+const getAverage = (name, ...scores) => {
+  console.log("name in function : ", name);
+  console.log("score in function : ", scores);
+  const average =
+    scores.reduce((total, scr) => {
+      total += scr;
+      return total;
+    }, 0) / scores.length;
+  console.log(average);
+};
+
+// getAverage(person.name, 89, 87, 100);
+const testScores = [23, 25, 67, 89];
+// getAverage(person.name, testScores); //this don't work
+getAverage(person.name, ...testScores);
+
+/*
+//Spread Operator :
+const boys = ["john", "petter", "bob"];
+const girls = ["susan", "anna"];
+
+const bestFriend = "arnold";
+
+// const friends = [boys, girls, bestFriend];
+// console.log(friends);//here we have nested array [Array(3), Array(2), 'arnold'
+
+const friends = [...boys, ...girls, bestFriend];
+console.log(friends);
+
+//reference :(change the original array)
+// const newFriends = friends;
+// newFriends[0] = "nancy";
+// console.log(newFriends);
+// console.log(friends);
+
+//copy :(with spread operator we have a copy of frinds)
+const newFriends = [...friends];
+newFriends[0] = "nancy";
+console.log(newFriends);
+console.log(friends);
+
+//object : (for when you don't want change original object)
+const person = { name: "john", job: "developer" };
+const newPerson = { ...person, city: "chicago" };
+const newPerson2 = person;
+// newPerson2.name = "kiya";
+console.log(newPerson);
+console.log(person);
+// console.log(newPerson2);
+*/
