9c423a73edbc7de32045028c0ab9c1bbbb97a744
Kiyarash <kiarashnadri@yahoo.com>
2023-06-09 16:52:43 +0330

    finish Javascript Nuggets

--- exercise2.js
+++ exercise2.js
@@ -96,26 +96,3 @@ let second = 'john';
 
 console.log(first, second)
 */
-//Debounce :
-const btn = document.querySelector('.btn');
-
-const debounce = (fn) => {
-  let timeoutID;
-  return () => {
-    console.log(timeoutID);
-    clearTimeout(timeoutID);
-    timeoutID = setTimeout(() => {
-      fn(); //here this fn() function run 2s after latest click on button
-    }, 2000);
-  };
-};
-
-btn.addEventListener(
-  'click',
-  debounce(() => {
-    console.log('you clicked me');
-  })
-);
-//delay logic
-//so it runs 2s after last click
-//setTimeout returns id,which pass into clearTimeout
